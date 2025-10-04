4975098d9f86e72dd50da826382307cbd82e9a23
Taras Protsyshyn <protsyshyn.t@gmail.com>
2025-10-04 16:46:30 +0000

    clear index file

--- src/index.ts
+++ src/index.ts
@@ -1,16 +1,1 @@
-import express, { Request, Response } from "express";
 
-import { connectToDatabase } from "./config/db.js";
-
-const app = express();
-const port = process.env.PORT ?? 3000;
-
-await connectToDatabase();
-
-app.get("/", (req: Request, res: Response) => {
-  res.send("Hello, TypeScript !!!");
-});
-
-app.listen(port, () => {
-  console.log(`Server running at http://localhost:${port.toString()}`);
-});
