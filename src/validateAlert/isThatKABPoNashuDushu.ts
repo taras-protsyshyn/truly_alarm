7fbca0e3daedcebf8fc471c6df974c3166b9fb7a
Taras Protsyshyn <protsyshyn.t@gmail.com>
2025-10-24 13:53:41 +0000

    fix validation about KAB

--- src/validateAlert/isThatKABPoNashuDushu.ts
+++ src/validateAlert/isThatKABPoNashuDushu.ts
@@ -2,6 +2,7 @@ import settlements from "../../assets/settlements.json" with { type: "json" };
 
 export function isThatKABPoNashuDushu(msg: string): boolean {
   return (
-    msg.includes("КАБ") && settlements.some((settlement) => msg.includes(settlement.toLowerCase()))
+    msg.includes("КАБ") &&
+    settlements.some((settlement) => msg.toLowerCase().includes(settlement.toLowerCase()))
   );
 }
